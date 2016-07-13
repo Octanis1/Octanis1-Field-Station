72c3344cf24dabcd9a4434e382c20fe91b11cb70
Octanis1 Field Station <info@octanis.org>
2016-07-13 07:57:24 +0000

    merging radio status changes, renaming scripts

--- src/bridge.py
+++ src/bridge.py
@@ -3,6 +3,7 @@
 """
 	Installation of paho : pip install paho-mqtt
 	Sorry, the name is ..._UPD_... instead of ..._UDP_...
+	line 41 and 69 : there are the two lines we need to decode base64 in the comments
 """
 
 import paho.mqtt.client as mqtt
@@ -12,7 +13,6 @@ import time
 import base64
 import json
 import ast
-import subprocess
 
 UDP_IP = "192.168.2.1"
 UDP_PORT_SENDER = 14555
@@ -41,15 +41,42 @@ def MQTT_to_UDP(client, userdata, msg):
 	sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 	sock.sendto(decodeData(msg.payload), (UDP_IP, UDP_PORT_RECIEVER))
-	
+	#sock.sendto(msg.payload, (UDP_IP, UDP_PORT_RECIEVER))
+
 def UDP_to_MQTT(client,data):
 	global topicToPublishMQTT
-	tmp=subprocess.call(["mosquitto_pub","-t "+topicToPublishMQTT,"-m "+decodeData(data),"-q 0"], shell=True)
-	#(result,mid)=client.publish(topicToPublishMQTT,data)
+	(result,mid)=client.publish(topicToPublishMQTT,data)
+
+""" the function which transform MQTT message into UDP message """
+"""def bridge_MQTT_to_UDP(name_thread,delay):
+	# I've tested and it works ! (use the script send_mqtt.sh and netcat to recieve the message)
+	client = mqtt.Client()
+	client.on_connect = on_connect
+	client.on_message = MQTT_to_UDP
+	client.connect(hostMQTT, port=portMQTT)
+	client.loop_forever()
+	time.sleep(delay)"""
+
+""" the function which transform UDP message into MQTT message """
+
+"""def bridge_UDP_to_MQTT(name_thread,delay):
+	# I've tested and it works ! (use the script send_udp.sh and mosquitto_sub to recieve the message)
+	global UDP_IP, UDP_PORT_SENDER
+	client = mqtt.Client()
+	client.connect(hostMQTT, port=portMQTT)
+	sock = socket.socket(socket.AF_INET, # Internet
+		                 socket.SOCK_DGRAM) # UDP
+	sock.bind((UDP_IP, UDP_PORT_SENDER))
+	while True:
+		data, addr = sock.recvfrom(1024) # buffer size is 1024 byte
+		#UDP_to_MQTT(client,decodeData(data))
+		UDP_to_MQTT(client,data)
+		time.sleep(delay)"""
 
 def callback_UDP_to_MQTT(sock,client,data):
 	data, addr = sock.recvfrom(1024) # buffer size is 1024 byte
-	UDP_to_MQTT(client,data)
+	UDP_to_MQTT(client,decodeData(data))
+	#UDP_to_MQTT(client,data)
 
 # Create the two threads
 try:
--- src/status_reporter.py
+++ src/status_reporter.py
@@ -3,6 +3,7 @@
 """
         Installation of paho : pip install paho-mqtt
         Sorry, the name is ..._UPD_... instead of ..._UDP_...
+        line 41 and 69 : there are the two lines we need to decode base64 in the comments
 """
 from pymavlink.dialects.v10 import common as mavlink
 import paho.mqtt.client as mqtt
@@ -31,11 +32,11 @@ class fifo(object):
 
 f = fifo()
 
-# Function definition is here
+# HEARTBEAT mavlink packet generator
 def gen_heartbeat_msg_str():
    # create a mavlink instance, which will do IO on file object 'f'
    mav = mavlink.MAVLink(f, 24, 1)
-   m = mav.heartbeat_encode(6,8,4,3,2)
+   m = mav.heartbeat_encode(0,8,4,3,2)
    m.pack(mav)
 
    # get the encoded message as a buffer
@@ -45,7 +46,8 @@ def gen_heartbeat_msg_str():
    
    return heartbeat_msg_str
 
-# Function definition is here
+
+# RADIO_STATUS mavlink packet generator
 def gen_radio_status_msg_str():
    # create a mavlink instance, which will do IO on file object 'f'
    mav = mavlink.MAVLink(f, 24, 1)
@@ -58,9 +60,8 @@ def gen_radio_status_msg_str():
    radio_status_str = base64.b64encode(b)
    # the devEUI is false. We have to modify it if we need the good one.
    radio_status_msg_str="{\"devEUI\":\"f03d291000000046\",\"fPort\":99,\"gatewayCount\":99,\"rssi\":99,\"data\":\""+ str(radio_status_str)  + "\"}"
-   
-   return radio_status_msg_str
 
+   return radio_status_msg_str
 
 
 mqttc=mqtt.Client()
@@ -69,7 +70,7 @@ mqttc.loop_start()
 
 while 1:
     (result,mid)=mqttc.publish(publishtopicMQTT, gen_heartbeat_msg_str())
-	(result,mid)=mqttc.publish(publishtopicMQTT, gen_radio_status_msg_str())
+    (result,mid)=mqttc.publish(publishtopicMQTT, gen_radio_status_msg_str())
     time.sleep(1)
 
 mqttc.loop_stop()
