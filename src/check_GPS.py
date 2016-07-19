0d26e64b56f6c3482fb34507d1159cd12db24891
jajoe <padawan_joseph@yahoo.fr>
2016-07-19 16:29:21 +0200

    put the script check GPS in an infinite loop

--- src/check_GPS.py
+++ src/check_GPS.py
@@ -5,7 +5,6 @@ import pymavlink.mavutil as mavutil
 import paho.mqtt.client as mqtt
 import socket
 import thread
-import base64
 import ast
 import sys 
 import binascii
@@ -13,7 +12,7 @@ import binascii
 hostMQTT="localhost"
 portMQTT=1883
 topicToPublishMQTT="application/70b3d57ed0000172/node/f03d291000000046/rx"
-timeMax = 50
+timeMax = 1 # we need timeMax <= 1 
 
 class fifo(object):
     def __init__(self):
@@ -28,35 +27,6 @@ class fifo(object):
 f = fifo()
 # create a mavlink instance, which will do IO on file object 'f'
 mav = mavlink.MAVLink(f)
-
-def encodeBase64(data):
-	return base64.b64encode(data)
-
-def encodeData(data):
-	data64=encodeBase64(data)
-	return "{\"reference\":\"mavlink\",\"devEUI\":\"f03d291000000046\",\"fPort\":1,\"confirmed\": false,\"data\":\""+str(data64)+"\"}"
-
-def isUBX(message):
-	if(len(message)>8):
-		if(ord(message[0]) == 181 and ord(message[1]) == 98):
-			return True
-		else:
-			return False
-	else:
-		return False
-
-def isSRV_IN(message):
-   if(ord(message[2]) == 1 and ord(message[3]) == 59):
-	   return True
-   else:
-	   return False
-	
-def isGPS_ready(message):
-   print(len(message))
-   if(ord(message[44])==0): # 44 because 6 of the header and 38 to have to good byte of the payload
-      return True
-   else:
-      return False
 
 def extract_observation_time(message): 
    if(len(message)>20):
@@ -98,26 +68,29 @@ ser = serial.Serial('/dev/ttyACM0')
 client = mqtt.Client()
 client.connect(hostMQTT, port=portMQTT)
 timeBegin=time.time()
-pollRequestGPS(ser)
 diffTime=0
-nbrEssai=3
-while (diffTime < timeMax) and (nbrEssai>0):
-   line=ser.readline()
+while 1:
+   # after waiting 0.5 second since the last timeBegin, we send another poll request and reinitialize the timer
    diffTime=time.time()-timeBegin
+   if(diffTime>timeMax):
+      # we wait 0.5 second, to avoid using 100% processor when the poll request always fail
+      time.sleep(0.5)
+      timeBegin=time.time()
+      pollRequestGPS(ser)
+
+   line=ser.readline()
    if(line[0] != '$'): # i.e we have a UBX message
       line=[str(i) for (x,i) in enumerate(line)]
       if(gps_ready(line)):
          publishMQTT_ready(client,line)
-         break
+         print("ready")
       else:
          publishMQTT_not_ready(client,line)
          pollRequestGPS(ser)
-         nbrEssai-=1
-         print("Not ready. We will do "+str(nbrEssai)+" other try")
-	
-if(diffTime>timeMax):
-   publishMQTT(client,False)
-   #publish sur le MQTT que c'est pas pret
+         print("Not ready")
+         # we wait 1 second and next we will send another poll request (because diffTime > 1 >= timeMax)
+      time.sleep(0.5)
+
 
 """
 ['0:\xb5', '1:b', '2:\x01', '3:;', '4:(', '5:\x00', '6:\x00', '7:\x00', '8:\x00', '9:\x00', '10:\x80', '11::', '12:$', '13:\x08', '14:\xc4', '15:\x0f', '16:\x00', '17:\x00', '18:\x00', '19:\x00', '20:\x00', '21:\x00', '22:\x00', '23:\x00', '24:\x00', '25:\x00', '26:\x00', '27:\x00', '28:\x00', '29:\x00', '30:\x00', '31:\x00', '32:\x00', '33:\x00', '34:\x00', '35:\xc2', '36:\x8b', '37:8', '38:\x00', '39:\x00', '40:\x00', '41:\x00', '42:\x00', '43:\x01', '44:\x00', '45:\x00', '46:\xa3', '47:s', '48:$', '49:G', '50:N', '51:R', '52:M', '53:C', '54:,', '55:1', '56:3', '57:5', '58:6', '59:1', '60:5', '61:.', '62:0', '63:0', '64:,', '65:V', '66:,', '67:,', '68:,', '69:,', '70:,', '71:,', '72:,', '73:1', '74:8', '75:0', '76:7', '77:1', '78:6', '79:,', '80:,', '81:,', '82:N', '83:,', '84:V', '85:*', '86:1', '87:5', '88:\r', '89:\n']
